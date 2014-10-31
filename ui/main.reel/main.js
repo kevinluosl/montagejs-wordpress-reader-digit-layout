e214e1b2a9369345fc811667252045b9d7ae2187
Jashon <Jashon.wan@siloon.com>
2014-10-31 16:58:04 +0800

    Jashon Wan

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -12,19 +12,56 @@ exports.Main = Component.specialize( /** @lends Main# */ {
 
     // "value": {"<-": "@rssController.articles.filter{!isRead}.length"}
     unreadLength:{
-        value:null
+        set: function(val) {
+            this._unreadLength=val;
+        },
+        get:function(){
+            return this._unreadLength;
+        }
     },
     ImgORText:{
-        value:null
+         set: function( val ) {
+            this._ImgORText = val;
+        },
+
+        get: function() {
+            return this._ImgORText;
+        }
     },
 	constructor: {
 		value: function Main() {
 			this.super();
 		}
 	},
-
-	templateDidLoad: {
-		value: function() {
+    handleAction:{
+        value: function(event){
+            if (event.target.identifier=="listBtn")
+            {
+                this.ImgORText="text";
+            }
+            else
+            {
+                this.ImgORText=null;
+            }
         }
+    },
+	templateDidLoad: {
+		value: null
+//            function() {
+//            var listenobj={};
+//            listenobj.handlelistBtnAction=function(event){
+//                debugger
+//            }
+//            var listbtn=document.querySelector("#listBtn");
+//            debugger
+//            listbtn.addEventListener("action",listenobj,false);
+//
+//            var listenflow={};
+//            listenflow.handleflowBtnAction=function(event){
+//                debugger
+//            }
+//            var flowbtn=document.querySelector("#flowBtn");
+//            flowbtn.addEventListener("action",listenflow,false);
+//        }
 	}
 } );
