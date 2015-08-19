f58563a6549f02c1024fc75edede72a5c25c4892
kevin.luo <kevin.luo@siloon.com>
2015-08-19 10:05:54 +0800

    Update to use montage-wordpress

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -2,22 +2,81 @@
  * @module ui/main.reel
  * @requires montage/ui/component
  */
-var Component = require( "montage/ui/component" ).Component;
+var Component = require("montage/ui/component").Component,
+    WordpressConnector = require("montage-wordpress/core/wordpress-connector").WordpressConnector;
 
 /**
  * @class Main
  * @extends Component
  */
-exports.Main = Component.specialize( /** @lends Main# */ {
-	constructor: {
-		value: function Main() {
-			this.super();
-		}
-	},
+exports.Main = Component.specialize(/** @lends Main# */ {
+    constructor: {
+        value: function Main() {
+            this.super();
+        }
+    },
 
-	templateDidLoad: {
-		value: function() {
+    _wordpressConnector: {
+        value: null
+    },
 
-		}
-	}
-} );
+    posts: {
+        value: null
+    },
+
+    categories: {
+        value: null
+    },
+
+    filterTerm: {
+        value: null
+    },
+
+    category: {
+        value: null
+    },
+
+    enterDocument: {
+        value: function (firstTime) {
+            var self = this;
+            if (firstTime) {
+                this._wordpressConnector.queryCategory().then(function (result) {
+                    var categories = [
+                        {name: 'Recent Posts', slug: '', count: '-1'}
+                    ];
+                    for (var i = 0; i < result.length; i++) {
+                        categories.push(result[i]);
+                    }
+                    self.categories = categories;
+                });
+                this._wordpressConnector.querySiteInfo();
+                this.addOwnPropertyChangeListener("filterTerm", this, false);
+                this.addOwnPropertyChangeListener("category", this, false);
+                this._wordpressConnector.addOwnPropertyChangeListener("posts", this, false);
+                //this._wordpressConnector.createUser('kevinluo','123456','Kevin');
+            }
+        }
+    },
+
+    handlePropertyChange: {
+        value: function (value, key) {
+            var self = this;
+            if (key === "posts" || key === "filterTerm") {
+                if (this._wordpressConnector.posts) {
+                    this._wordpressConnector.posts = this._wordpressConnector.posts.filter(function (post) {
+                        return !self.filterTerm ||
+                            post.title.toLowerCase()
+                                .indexOf(self.filterTerm.toLowerCase()) >= 0;
+                    });
+                } else {
+                    this._wordpressConnector.posts = [];
+                }
+            } else if (key === 'category') {
+                this._wordpressConnector.queryPosts(this.category).then(function(result){
+                    self.posts = result;
+                });
+            }
+        }
+    }
+
+});
